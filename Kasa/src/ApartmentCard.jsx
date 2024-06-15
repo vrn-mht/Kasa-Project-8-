065aaf6fb58ffabd8b5f076e53b508c6258fd732
Veron Mahamat <oumar.mht243@gmail.com>
2024-06-15 12:53:17 +0200

    Ajout dynamic img and title apartment

--- Kasa/src/ApartmentCard.jsx
+++ Kasa/src/ApartmentCard.jsx
@@ -3,13 +3,15 @@ import './ApartmentGrid.scss'
 import './ApartmentCard.scss'
 import {NavLink} from "react-router-dom"
 
-function ApartementCard() {
+function ApartementCard(props) {
+  console.log("props dans apartment card:", props);
   return (
-    <div className='apartment'> 
     <NavLink to ="/flat">
-      <div className='apartment_subtitle'>Titre de la location</div>
-    </NavLink>
+    <div className='apartment'>
+      <img src={props.imageUrl} alt="" /> 
+      <div className='apartment_subtitle'>{props.title}</div>
     </div>
+    </NavLink>
   )
 }
 
--- Kasa/src/ApartmentGrid.jsx
+++ Kasa/src/ApartmentGrid.jsx
@@ -1,16 +1,27 @@
 
 import './ApartmentGrid.scss'
 import ApartmentCard from "./ApartmentCard.jsx"
+import { useEffect, useState } from 'react';
+
 
 function ApartmentGrid() {
+  const [apartments, setApartments] = useState([]);
+
+  useEffect(fetchApartments, []);
+
+  function fetchApartments() {
+    fetch("db.json")
+    .then((res) => res.json())
+    .then(res => setApartments(res))
+    .catch(console.error); 
+  }
+
+
   return (
     <div className='grid' >
-      <ApartmentCard />
-      <ApartmentCard />
-      <ApartmentCard />
-      <ApartmentCard />
-      <ApartmentCard />
-      <ApartmentCard />
+      {apartments.map((apartment) => (
+      <ApartmentCard title = {apartment.title} imageUrl={apartment.cover} />
+         ))}
     </div>
   )
 }
